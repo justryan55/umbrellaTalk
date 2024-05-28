32d6220dea271163fdf2afe6c99bd3379427e8e0
Ryan <ryan.irani@gmail.com>
2024-05-28 08:26:40 +0100

    added preventDefault

--- client/src/components/LoginRegisterButton.jsx
+++ client/src/components/LoginRegisterButton.jsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
 
 export default function LoginRegisterButton({action}) {
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
   return (
     <div>
         <button
             type="submit"
+            onSubmit={handleSubmit}
             action = {action}
             className="flex w-full justify-center rounded-md bg-orange-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"
             >
