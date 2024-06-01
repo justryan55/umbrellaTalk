3e4f517a15cd582798243e6f6e733b1797ae81a5
Ryan <ryan.irani@gmail.com>
2024-06-01 19:32:56 +0100

    added logout button

--- client/src/components/LogoutButton.jsx
+++ client/src/components/LogoutButton.jsx
@@ -24,12 +24,24 @@ export default function LogoutButton() {
     }
  
   return (
-    <div>
-        <img src={logoutIcon} 
-             alt='logout-icon' 
-             onClick={() => handleLogout()}
-        />
-    </div>
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="#FFF"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="nav-icon"
+        id="logout-icon"
+        onClick={handleLogout}
+    >
+        <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
+        <polyline points="16 17 21 12 16 7"></polyline>
+        <line x1="21" y1="12" x2="9" y2="12"></line>
+    </svg>  
 
 )
 }
