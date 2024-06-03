4638600a605281dafa3338a4ecd8145751f98f98
Ryan <ryan.irani@gmail.com>
2024-06-03 11:25:12 +0100

    added navigate to p text

--- client/src/components/CreateNewMessageButton.jsx
+++ client/src/components/CreateNewMessageButton.jsx
@@ -35,7 +35,6 @@ export default function CreateNewMessageButton() {
                 )
                 
 
-                console.log(userListComponents)
 
                 setUserListComponents(users)
 
--- client/src/components/HeaderBar.jsx
+++ client/src/components/HeaderBar.jsx
@@ -1,14 +1,20 @@
 import { useContext } from "react"
 import { UserContext } from "../services/AuthContext"
 import CreateNewMessageButton from "./CreateNewMessageButton"
+import { useNavigate } from "react-router"
 
 
 export default function HeaderBar() {
     const [user, setUser] = useContext(UserContext)
+    const navigate = useNavigate()
+
+    const handleClick = () => {
+      navigate('/dashboard')
+    }
 
   return (
     <div className="header-bar-content">
-        <p className="header-text">Welcome {user.name}</p>
+        <p className="header-text" onClick={handleClick}>Welcome {user.name}</p>
         <CreateNewMessageButton />
     </div>
   )
