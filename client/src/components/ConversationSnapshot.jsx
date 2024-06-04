09ca7a9fa0a81cbf6f8336629c5a02107504a68b
Ryan <ryan.irani@gmail.com>
2024-06-04 21:58:42 +0100

    added conversation snapshot function

--- client/src/components/ConversationSnapshot.jsx
+++ client/src/components/ConversationSnapshot.jsx
@@ -1,24 +1,49 @@
 import { useNavigate } from 'react-router'
 import profilePicture from '../assets/images/profile-picture.png'
 import tick from '../assets/images/tick.svg'
+import { useState } from 'react';
 
-const ConversationSnapshot = () => {
-  const navigate = useNavigate()
+const ConversationSnapshot = (message) => {
+  const navigate = useNavigate();
+  const [senderName, setSenderName] = useState("")
 
   const handleClick = () => {
-    navigate('/conversation')
-  }
+    navigate('/conversation');
+  };
+
+  const getUserList = async () => {
+    const res = await fetch("http://localhost:5000/api/users", {
+      method: "GET", 
+      headers: {'Content-Type': 'application/json'}
+    });
+    const data = await res.json();
+
+    const userNames = data.user.map((user) => {
+      if (user._id === message.message.sender){
+        setSenderName(user.name)
+      }
+
+    })
+
+  };
+
+  getUserList()
+
+ 
+
+
+
 
   return (
     <div className='conversation-snapshot-container' onClick={() => handleClick()}>
         <div className='snapshot-content'>
             <img src={profilePicture} alt='profile' className='snapshot-profile-image'/>
             <div className='snapshot-first-row'>
-                <p className='snapshot-last-user'>Ryan Irani</p>
-                <p className='snapshot-last-message'>How are you?</p>
+                <p className='snapshot-last-user'>{senderName}</p>
+                <p className='snapshot-last-message'>{message.message.message}</p>
             </div>
             <div className='snapshot-second-row'>
-                <p className='snapshot-timestamp'>1 hour ago</p>
+                <p className='snapshot-timestamp'>{message.message.createdAt}</p>
                 
                 <img src={tick} alt='sent' className='snapshot-message-sent-image'/>
             </div>
