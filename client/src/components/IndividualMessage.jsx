c68d69ae4fb36d81d5f8194028f7c05e91c95d1b
Ryan <ryan.irani@gmail.com>
2024-06-15 13:26:58 +0100

    added edit function to the react component

--- client/src/components/IndividualMessage.jsx
+++ client/src/components/IndividualMessage.jsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from 'react'
 import profilePicture from '../assets/images/profile-picture.png'
 
 export default function MessageSnapshot({messageDetails, own}) {
-
-  const message = messageDetails.message;
+  const [message, setMessage] = useState('')
+  const [isEditing, setIsEditing] = useState(false)
   const timestamp = messageDetails.createdAt
+
+  const handleEditClick = () => {
+    setIsEditing(true)
+  }
+
+  const handleTextAreaChange = (e) => {
+    setMessage(e.target.value)
+  }
+  
+  const handleTextAreaKeyDown = (e) => {
+    if (e.keyCode === 13){
+      setIsEditing(false)
+    }
+  }
+
+  useEffect(() => {
+      setMessage(messageDetails.message)
+      console.log(messageDetails)
+  }, [messageDetails])
 
   return (
     <div className={own ? 'outgoing-message' : 'incoming-message'}>
@@ -11,8 +31,18 @@ export default function MessageSnapshot({messageDetails, own}) {
             <div className='message-top'>
                 <img className='message-img' src={profilePicture} alt='' />
                 <div className='message-text-container'>
-                  <p className='message-text'>{message}</p>
-                  {own ? 
+                  {isEditing ? (
+                    <textarea
+                      value={message}
+                      onChange={handleTextAreaChange}
+                      onKeyDown={handleTextAreaKeyDown}
+                      onBlur={() => setIsEditing(false)}
+                      autoFocus
+                    />
+                  ) : (
+                    <p className="message-text">{message}</p>
+                  )}
+                  {own && !isEditing && ( 
                   <svg xmlns="http://www.w3.org/2000/svg" 
                         width="24" 
                         height="24" 
@@ -22,10 +52,12 @@ export default function MessageSnapshot({messageDetails, own}) {
                         strokeWidth="2" 
                         strokeLinecap="round" 
                         strokeLinejoin="round" 
-                        className="edit-icon">
+                        className="edit-icon"
+                        onClick={handleEditClick}
+                        >
                         <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path>
                   </svg> 
-                  : null}
+                  )}
                 </div>
           </div>
         )}
