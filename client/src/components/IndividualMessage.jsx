03b7865c27e6caf9d82a0b5f0a7478695218c7a9
Ryan <ryan.irani@gmail.com>
2024-06-17 19:00:18 +0100

    updated trash icon to individual message

--- client/src/components/IndividualMessage.jsx
+++ client/src/components/IndividualMessage.jsx
@@ -58,20 +58,36 @@ export default function MessageSnapshot({messageDetails, own}) {
                     <p className="message-text">{message}</p>
                   )}
                   {own && !isEditing && ( 
-                  <svg xmlns="http://www.w3.org/2000/svg" 
-                        width="24" 
-                        height="24" 
-                        viewBox="0 0 24 24" 
-                        fill="none" 
-                        stroke="black" 
-                        strokeWidth="2" 
-                        strokeLinecap="round" 
-                        strokeLinejoin="round" 
-                        className="edit-icon"
-                        onClick={handleEditClick}
-                        >
-                        <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path>
-                  </svg> 
+                  <div className='icon-container'>
+                    <svg xmlns="http://www.w3.org/2000/svg" 
+                         width="24" 
+                         height="24" 
+                         viewBox="0 0 24 24" 
+                         fill="none" 
+                         stroke="black" 
+                         strokeWidth="2" 
+                         strokeLinecap="round" 
+                         strokeLinejoin="round" 
+                         className="bin-icon">
+                         <polyline points="3 6 5 6 21 6"></polyline>
+                         <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+                    </svg>
+
+                    <svg xmlns="http://www.w3.org/2000/svg" 
+                          width="24" 
+                          height="24" 
+                          viewBox="0 0 24 24" 
+                          fill="none" 
+                          stroke="black" 
+                          strokeWidth="2" 
+                          strokeLinecap="round" 
+                          strokeLinejoin="round" 
+                          className="edit-icon"
+                          onClick={handleEditClick}
+                          >
+                          <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path>
+                    </svg> 
+                  </div>
                   )}
                 </div>
           </div>
