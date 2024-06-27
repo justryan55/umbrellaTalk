9f6ae5df74746dcee1a2888a3a7c57146bcc513a
Ryan <ryan.irani@gmail.com>
2024-06-27 23:33:43 +0100

    updated cors code

--- server/server.cjs
+++ server/server.cjs
@@ -28,24 +28,13 @@ const connectDatabase = async () => {
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-const allowedOrigins = ['https://umbrella-talk-client.vercel.app'];
-
-app.use(
-    cors({
-        origin: function (origin, callback) {
-            if (!origin || allowedOrigins.includes(origin)) {
-                callback(null, true);
-            } else {
-                callback(new Error('Not allowed by CORS'));
-            }
-        },
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type', 'Authorization'],
-        credentials: true 
-    })
-);
-
-
+app.use(cors({
+    origin: 'https://umbrella-talk-client.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+  }))
+  
 app.options('*', cors());
 
 app.post("/api/auth/register", [
