d82d369969323a805699fcef58b84d6ef29eccb3
Ryan <ryan.irani@gmail.com>
2024-06-27 23:23:02 +0100

    updated cors url

--- server/server.cjs
+++ server/server.cjs
@@ -29,7 +29,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use(cors({
-    origin: 'https://umbrella-talk-client.vercel.app/',
+    origin: 'https://umbrella-talk-client.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
